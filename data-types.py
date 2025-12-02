ff3846e0bdaab9ae5bf11eaf745d6cb21447fc14
Pramodh Kumar <pramodhkumar0202@gmail.com>
2025-12-02 09:08:31 +0530

    added

---
+++ data-types.py
@@ -0,0 +1,29 @@
+# in python snake_case is the preferred
+
+text   = 'Text in python is called String'
+text_2 = "It has to be inside single or double quotes"
+text_3 = 'We can use "double-quotes" inside single quotes'
+text_4 = "And 'vice-versa' " # This is a string
+
+print(text)
+print(text_2)
+print(text_3)
+print(text_3)
+
+# Basic Data-Types in Python
+
+string    = 'text'
+num_int   = 10
+num_float = 3.14
+boolean   = True #False
+none_type = None
+
+# string formatting
+name = 'pramod'
+age  = '29'
+
+# After python 3.6.6 - f-string or .format()
+print(f"My Name is {name} and I'm {age} years old")
+
+# Before Python 3.6.6 - .format()
+print("My Name is {} and I'm {} years old".format(name, age))
