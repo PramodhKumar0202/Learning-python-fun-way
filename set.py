6913ccc86db73215d6d45473969925f0e37c10d6
Pramodh Kumar <pramodhkumar0202@gmail.com>
2025-12-12 22:31:39 +0530

    added

--- set.py
+++ set.py
@@ -29,3 +29,25 @@ items.clear()                # Remove all elements
 items.remove(3)              # Remove element (error if missing)
 items.discard(3)             # Remove element (no error if missing)
 items.add(10)                # Add a single element
+
+#Set Methods: Compare Sets
+#-------------------------
+a = {1,2,3,4}
+b = {3,4,5,6}
+
+print(a.union(b))                # Join 2 Sets
+print(a.intersection(b))         # Find Items in BOTH sets
+print(a.difference(b))           # Set A items that are missing in set B
+print(a.symmetric_difference(b)) # Symmetric difference between 2 sets
+print(a.issubset(b))             # Is set a completely inside set b?
+print(a.issuperset(b))           # Does set_a contain all from set_B?
+
+#Popular Functions with Lists
+#-------------------------
+set_nums = {10, 30, 20, 50, 40 , 10, 20}
+
+print(len(set_nums ))     # Get Length             > 5
+print(sorted(set_nums ))  # Create new sorted list > [10,20,30,40,50]
+print(sum(set_nums ))     # Sum Numbers            > 150
+print(min(set_nums ))     # Get Min Value          > 10
+print(max(set_nums ))     # Get Max Value          > 50
